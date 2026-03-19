2a4e56f60fb91d361cba11f2b03292f527e61cf9
Hector Zambrano <elderzambranoperez@gmail.com>
2026-03-19 02:55:23 -0300

    done

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,13 @@
+using System;
+class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} at {_company} from {_startYear} to {_endYear}");
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Senior Software Engineer";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+
+        resume myResume = new resume();
+        myResume._name = "Hector Zambrano";
+        myResume._jobs = new List<Job>();
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
